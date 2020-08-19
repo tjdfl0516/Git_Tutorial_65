b3d49e47205fa90a78d61dd8dcbe7703492d8041
tjdfl0516 <tjdfl0516@naver.com>
2020-08-19 12:16:22 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -16,4 +16,12 @@ public class UserController
 
    return "user/insert";
  }
+
+ // 회원수정 페이지로 이동
+ @RequestMapping("/user/update")
+ public String update() {
+   logger.info("경로 : update")
+
+   return "user/update";
+ }
 }
